3e475e398c62057437168af9b455c0d41fe36833
cafferatielisa <cafferati.elisa@iisgalvanimi.edu.it>
2023-05-24 10:59:52 +0000

    nona opra

--- import_data.py
+++ import_data.py
@@ -7,30 +7,26 @@ mydb = mysql.connector.connect(
   user="root",
   password=""
 )
+
 mycursor = mydb.cursor()
 
 mycursor.execute("CREATE DATABASE IF NOT EXISTS FORMULA1")
 
-mycursor.execute("DROP TABLE FORMULA1.piloti")
-
 mycursor.execute("""
-  CREATE TABLE IF NOT EXISTS FORMULA1.piloti (
+  CREATE TABLE IF NOT EXISTS FORMULA1.driver_standings (
+    driverStandingsId INTEGER,
+    raceId INTEGER,
     driverId INTEGER,
-    driverRef VARCHAR(30) NOT NULL,
-    number VARCHAR(30),
-    code VARCHAR(30),
-    forename VARCHAR(30),
-    surname VARCHAR(30),
-    dob DATE,
-    nationality VARCHAR(30)
+    points INTEGER,
+    position INTEGER,
+    positionText VARCHAR(30),
+    wins INTEGER
   );""")
 
-
-mycursor.execute("DELETE FROM FORMULA1.piloti")
 mydb.commit()
 
 #Read data from a csv file
-formula_data = pd.read_csv('./drivers.csv', index_col=False, delimiter = ',')
+formula_data = pd.read_csv('./driver_standings.csv', index_col=False, delimiter = ',')
 formula_data = formula_data.fillna('Null')
 formula_data.drop(formula_data.columns[len(formula_data.columns)-1], axis=1, inplace=True)
 print(formula_data.head(20))
@@ -39,14 +35,14 @@ print(formula_data.head(20))
 for i,row in formula_data.iterrows():
     cursor = mydb.cursor()
     #here %S means string values 
-    sql = "INSERT INTO FORMULA1.piloti VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+    sql = "INSERT INTO FORMULA1.driver_standings VALUES (%s,%s,%s,%s,%s,%s,%s)"
     cursor.execute(sql, tuple(row))
     print("Record inserted")
     # the connection is not auto committed by default, so we must commit to save our changes
     mydb.commit()
 
 
-mycursor.execute("SELECT * FROM FORMULA1.piloti")
+mycursor.execute("SELECT * FROM FORMULA1.driver_standings")
 myresult = mycursor.fetchall()
 
 for x in myresult:
