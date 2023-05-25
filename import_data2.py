de5c0521b51a41e4da0851c153be741a0179c88e
cafferatielisa <cafferati.elisa@iisgalvanimi.edu.it>
2023-05-25 11:00:38 +0000

    decima ora

--- import_data2.py
+++ import_data2.py
@@ -13,24 +13,24 @@ mycursor = mydb.cursor()
 #Create the DB (if not already exists)
 mycursor.execute("CREATE DATABASE IF NOT EXISTS FORMULA1")
 
-mycursor.execute("DROP TABLE IF EXISTS FORMULA1.piloti")
+mycursor.execute("DROP TABLE IF EXISTS FORMULA1.circuits")
 
 #Create the table for the csv data (if not exists)
 mycursor.execute("""
-  CREATE TABLE IF NOT EXISTS FORMULA1.piloti (
-    driverId INTEGER,
-    driverRef VARCHAR(30) NOT NULL,
-    number TEXT,
-    code VARCHAR(30),
-    forename VARCHAR(30),
-    surname VARCHAR(30),
-    dob DATE,
-    nationality VARCHAR(30),
+  CREATE TABLE FORMULA1.circuits(
+    circuitId INTEGER,
+    circuitRef VARCHAR(30) NOT NULL,
+    name TEXT,
+    location VARCHAR(30),
+    country VARCHAR(30),
+    lat INTEGER,
+    lng INTEGER,
+    alt TEXT,
     url TEXT
   );""")
 
 #Read data from a csv file
-formula_data = pd.read_csv('./drivers.csv', index_col=False, delimiter = ',')
+formula_data = pd.read_csv('./circuits.csv', index_col=False, delimiter = ',')
 formula_data = formula_data.fillna('Null')
 print(formula_data.head(20))
 
@@ -38,14 +38,14 @@ print(formula_data.head(20))
 for i,row in formula_data.iterrows():
     cursor = mydb.cursor()
     #here %S means string values 
-    sql = "INSERT INTO FORMULA1.piloti VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    sql = "INSERT INTO FORMULA1.circuits VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
     cursor.execute(sql, tuple(row))
     print("Record inserted")
     # the connection is not auto committed by default, so we must commit to save our changes
     mydb.commit()
 
 #Check if the table has been filled
-mycursor.execute("SELECT * FROM FORMULA1.piloti")
+mycursor.execute("SELECT * FROM FORMULA1.circuits")
 myresult = mycursor.fetchall()
 
 for x in myresult:
